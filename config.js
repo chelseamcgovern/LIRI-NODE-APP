4bc8d10a4fa1e59c32d0864ccde213f7475b67fb
chelseamcgovern <31602839+chelseamcgovern@users.noreply.github.com>
2018-04-07 17:22:27 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'eRzsDX1ahrUgxakr5jdVubAQx',
+    consumer_secret:      'qGNglMm6UUlbL6wwo6xz5fGoior20sOQev3ld0qK7ghzuR045c',
+    access_token:         '981372599241531392-FXYoJudvs2DSSAsO1EHA8bZtwR4gFfm',
+    access_token_secret:  '306SX8ihRc5HPbMkbEJXo5FOsNGdtpN0AVhyBPd7VlRRT',
+}
